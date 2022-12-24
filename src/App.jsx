5b1f965efee3252bbbb72dc5662b24beecd334d2
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2022-12-24 17:02:15 +0530

    portal completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Canvas } from "@react-three/fiber";
+
+import SceneContainer from "./Components/SceneContainer";
+
+function App() {
+  return (
+    <Canvas>
+      <SceneContainer />
+    </Canvas>
+  );
+}
+
+export default App;
